eb971389a258089b29269c161467766fc6a1d355
Arel <arel.hello@gmail.com>
2019-08-17 23:54:29 +0800

    Complete lottery logic.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log('hi')
+$(document).ready(function(){
+
+  // Click fake-lottery to generate fake prize
+  $('#fake-lottery-link').click(function() {
+    $('#popup-backdrop').toggleClass('hide');
+    $('#loading').toggleClass('hide');
+    setTimeout(function() {
+      $('#loading').toggleClass('hide');
+      $('#first-prize').toggleClass('hide');
+    }, 3000)
+  })
+
+  // Click popup-backdrop to hide
+  $('#popup-backdrop').click(function(e) {
+    $(this).toggleClass('hide')
+    $('#first-prize').toggleClass('hide');
+  })
+
+  // Click popup itself won't close the popup
+  $('#popup').click(function(e) {
+    e.stopPropagation()
+  })
+
+});
